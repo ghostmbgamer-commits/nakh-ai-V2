9bc3190db1229dad4481d247ee531ab3b6c82e11
ghostmbgamer-commits <ghostmbgamer@gmail.com>
2026-02-25 08:26:48 +0330

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nakhai.app',
+  appName: 'Nakh AI',
+  webDir: 'dist'
+};
+
+export default config;
